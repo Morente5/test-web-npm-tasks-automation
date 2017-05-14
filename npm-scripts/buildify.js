5f4c1646286514433470fcbfb5557397d9c7d3a4
Alejandro Morente <morente5@hotmail.es>
2017-05-14 21:14:36 +0200

    Optimisation

--- npm-scripts/buildify.js
+++ npm-scripts/buildify.js
@@ -1,7 +1,5 @@
 const  buildify = require('buildify'),
 	filesJS = [
-		'./node_modules/jquery/dist/jquery.min.js',
-		'./node_modules/sticky-kit/dist/sticky-kit.js',
 		'./dev/js/forms.js',
 		'./dev/js/map.js',
 		'./dev/js/mobile-menu.js',
@@ -11,14 +9,13 @@ const  buildify = require('buildify'),
 	filesCSS = [
 		'./dev/css/styles.css'
 	];
-
-	buildify()
-		.concat( filesCSS )
-		.cssmin()
-		.save( './dist/css/styles.css' );
-
 	buildify()
 		.concat( filesJS )
 		.save( './dist/js/script.js' )
 		.uglify()
 		.save( './dist/js/script.min.js' );
+
+	buildify()
+		.concat( filesCSS )
+		.cssmin()
+		.save( './dist/css/styles.css' );
